7b970ad18707e914fac435a78092dec7b96bd222
FMXavier <flavio.mx@gmail.com>
2021-10-21 20:00:48 -0300

    Delete provider.tf

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "oci" {
-   region = "sa-saopaulo-1"
-   tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaxy5dlfx6zbydjsaojekyrikj2vpsuufhyaeyc2ysdhpuwqw7f27a"
-   user_ocid = "ocid1.user.oc1..aaaaaaaawtjhmqechz6jysrawiqvqgrqzqhq7dtfyopknobm32r45vfpegca"
-   fingerprint = "51:e3:bc:af:6f:15:9b:b8:3b:aa:d0:e7:34:72:53:8f"
-   private_key_path = "./oracleidentitycloudservice_flavio.mx-10-20-18-37.pem"
-}
